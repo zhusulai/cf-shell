7398e6ed58561786f28de7f3ad1fcd3bbbd27a48
John Zhu <john.s.zhu@rbc.com>
2018-08-24 11:33:05 -0400

    - added devault values to arguments - added windows builds

--- xcompile.sh
+++ xcompile.sh
@@ -5,8 +5,11 @@ FILE=$2
 VERSION=$3
 
 if [ "$OUTPUTDIR" = "" -o "$FILE" = "" -o "$VERSION" = "" ]; then
-   echo "Requires output directory, output filename, and version as input."
-   exit 1
+#    echo "Requires output directory, output filename, and version as input."
+#    exit 1
+    OUTPUTDIR="builds"
+    FILE="cf-shell"
+    VERSION="current"
 fi
 
 mkdir -p $OUTPUTDIR/$VERSION
@@ -16,7 +19,7 @@ rc=`echo $?`
 if [ $rc -ne 0 ]; then
    echo "linux64 build failed"
    exit 1
-fi 
+fi
 
 GOOS=linux GOARCH=386 go build -o $OUTPUTDIR/$VERSION/${FILE}.linux32
 if [ $rc -ne 0 ]; then
@@ -28,4 +31,16 @@ GOOS=darwin GOARCH=amd64 go build -o $OUTPUTDIR/$VERSION/${FILE}.osx
 if [ $rc -ne 0 ]; then
    echo "darwin build failed"
    exit 1
-fi 
+fi
+
+GOOS=windows GOARCH=amd64 go build -o $OUTPUTDIR/$VERSION/${FILE}.win64.exe
+if [ $rc -ne 0 ]; then
+   echo "windows x64 build failed"
+   exit 1
+fi
+
+GOOS=windows GOARCH=386 go build -o $OUTPUTDIR/$VERSION/${FILE}.win32.exe
+if [ $rc -ne 0 ]; then
+   echo "windows x32 build failed"
+   exit 1
+fi
