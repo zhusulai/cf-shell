d1b376f52aea342f222fd77ab02653888e05eaa6
John Zhu <john.s.zhu@rbc.com>
2018-08-24 16:36:42 -0400

    - added support for app names and service names

--- cfcli/resolver.go
+++ cfcli/resolver.go
@@ -4,22 +4,46 @@ import (
 	"strings"
 )
 
-func StartsWithResolvableKeyWord(usage string) bool {
-	switch strings.Split(usage, " ")[0] {
-	case "SPACE":
-		return true
-	case "ORG":
-		return true
+var resolverKeyword = []string{
+	"APP_NAME",
+	//"BUILDPACK",
+	//"BUILDPACK_NAME",
+	//"DOMAIN",
+	//"FEATURE_NAME",
+	"ORG",
+	"ORG_NAME",
+	//"PLUGIN-NAME",
+	//"QUOTA",
+	//"SECURITY_GROUP",
+	//"SEGMENT_NAME",
+	//"SERVICE_BROKER",
+	"SERVICE_INSTANCE",
+	//"SOURCE_APP",
+	"SPACE",
+	//"SPACE_QUOTA",
+	//"SPACE_QUOTA_NAME",
+	//"STACK_NAME",
+}
+
+func IsResolvableKeyWord(word string) bool {
+	for _, keyword := range resolverKeyword {
+		if strings.EqualFold(word, keyword) {
+			return true
+		}
 	}
 	return false
 }
 
-func ResolveKeyWord(usage string) []string {
-	switch strings.Split(usage, " ")[0] {
+func ResolveKeyWord(word string) []string {
+	switch word {
+	case "APP_NAME":
+		return context.cache.Apps()
+	case "ORG", "ORG_NAME":
+		return context.cache.Orgs()
+	case "SERVICE_INSTANCE":
+		return context.cache.Services()
 	case "SPACE":
 		return context.cache.Spaces()
-	case "ORG":
-		return context.cache.Orgs()
 	}
 	return nil
 }
